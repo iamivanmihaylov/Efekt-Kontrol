aa70692edd2f8da802a282785110a604ac7abf28
iamivanmihaylov <ivan4o03.it@gmail.com>
2022-01-08 23:33:37 +0200

    Add gulp. Add home index page.

---
+++ src/Web/EfektKontrol.Web/gulpfile.js
@@ -0,0 +1,13 @@
+﻿/// <binding AfterBuild='sass-compile' />
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+
+gulp.task('sass-compile', async function () {
+    gulp.src('./wwwroot/styles/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./wwwroot/css'));
+});
+
+gulp.task('watch-sass', async function () {
+    gulp.watch('./wwwroot/styles/*.scss', gulp.series('sass-compile'));
+});
